19c7249b05727f92a44aa3fc1661aba42492569d
Zachary Connor <108571267+zacbookpromd@users.noreply.github.com>
2022-07-02 14:51:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+
+def get_drvier():
+  #Set options to make browsing easier
+  options = webdriver.ChromeOptions()
+  options.add_argument("disable-infobars")
+  options.add_argument("start-maximized")
+  options.add_argument("disable-dev-shm-usage")
+  options.add_argument("no-sandbox")
+  options.add_experimental_option("excludeSwitches",
+  ["enable-automation"])
+  options.add_argument
+  ("disable-blink-features-AutomationControlled")
+  
+  driver = webdriver.Chrome(options=options)
+  driver.get("http://automated.pythonanywhere.com")
+  return driver
+
+def main():
+  driver = get_drvier()
+  element = driver.find_element(by="xpath",value="/html/body/div[1]/div/h1[1]")
+  return element.text
+
+print(main())
